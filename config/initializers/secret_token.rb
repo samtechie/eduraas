36b72805862493810ab06418b5f4f5fb75a89a8e
Wasswa Samuel <latimerscope@gmail.com>
2013-05-27 22:36:12 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eduraas::Application.config.secret_token = 'ed327602388a7a2cf661971afcdeb2e21ea4c5e45372de9f777d3aacd72f071ca8a5557c72fd5688f1ded4c3a41d30ec0bee90296c19fe80e8029d287734d14f'
